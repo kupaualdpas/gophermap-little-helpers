38dc7bd078d394362f2114be6e87ab1a6a28c8c1
Stefan Straubinger <kupaualdpas@gmail.com>
2016-11-17 22:19:38 +0100

    Initial commit

---
+++ ZeichenketteSuchen.c
@@ -0,0 +1,81 @@
+/* file: ZeichenketteSuchen.c
+ *
+ * Copyright (C) am 6.November2016 Stefan Straubinger
+ *
+ * A simple "Find and Replace" piece of code.
+ * Usefull for changing IP_address in a gophermap file.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAXLEN 120       /* maximum length of Input Line*/
+
+#define DEBUG 0
+
+
+long mygetline(char s[], long lim);
+
+int main()
+{
+    int i;
+    char * needle = "127.0.0.1";            // searching for needle
+    char IP_address []= "44.143.58.106";    // replace needle with IP_address
+    char haystack [MAXLEN] = "";            // a line of the input text,
+    char buffer [MAXLEN] = "";              // copy of input line
+    char leer [MAXLEN] = "";
+    char * locator;
+
+
+    //printf("Replace String with another!\n");
+
+    while(mygetline(haystack, MAXLEN) ) {
+        while (locator = strstr(haystack,needle))
+        {
+            if (DEBUG) printf("-----------------------------------------\n%p\n--->%s<---\n",locator, locator);
+            if (DEBUG) printf("%s", haystack);
+
+            if (strlen(needle) == strlen(IP_address))
+                strncpy(locator, IP_address, strlen(IP_address));
+            if (strlen(needle) > strlen(IP_address)) {
+                strncpy(locator, IP_address, strlen(IP_address));
+                strcpy(buffer, locator);
+                strcpy(locator+strlen(IP_address), buffer + strlen(needle));
+            }
+            if (strlen(needle) < strlen(IP_address)) {
+                strcpy(buffer, locator);
+                memmove(locator + (strlen(IP_address) - strlen(needle)),buffer,strlen(buffer) );
+                strncpy(locator, IP_address, strlen(IP_address));
+            }
+            for (i=0; i< MAXLEN; i++) //delete buffer.
+                buffer[i] = '\0';
+
+        }
+        printf("%s", haystack);
+
+    }
+    printf("\n");
+
+    return 0;
+}
+
+
+
+/* getline: read a line into s
+ * return: length, is 0 if EOF
+ */
+long mygetline(char s[], long lim) {
+  long c, i;
+  for (i = 0; (i < lim - 1) && ((c=getchar()) != EOF) && (c != '\n'); i++) {
+    s[i] = c;
+  }
+
+  if (c == '\n') {
+    s[i] = c;
+    i++;
+  }
+
+  s[i] = '\0';
+  return i;
+} // end mygetline
